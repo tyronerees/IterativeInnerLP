c16d39b8241725abc4604c5b11fd08b60de68899
Tyrone Rees <tyrone.rees@stfc.ac.uk>
2015-08-07 14:15:56 +0100

    Separated out the test loop from the driver

---
+++ lp_test_loop.m
@@ -0,0 +1,68 @@
+%lp_test_loop.m
+% 
+% requires 'test = struct('Method',{},...
+%              'krylov_method',{},...
+%             'precond_method',{},...
+%             'inner_tol',{},...
+%             'PDConvergenceDescrptions',{},...
+%             'descriptions',{});' 
+% and 'problem' (from netlib) structures to be defined
+% together with scalars length_problem, max_sigma, no_solvers
+
+
+for i = 1:length_problem
+
+    %!!!!!!!!!!!!!!!!!!!!!!!!!!%
+    %! Loop over the problems !%
+    %!!!!!!!!!!!!!!!!!!!!!!!!!!%
+    probstring = ['/numerical/trees/matrices/netlib_problems/',problem{i}];
+    %    load(problem{i});
+    load(probstring);
+    
+    lp_setup;
+    
+    max_sigma(i) = svds(A,1)
+    
+    conv_data.max_sigma = max_sigma(i);
+    options.conv_data = conv_data;
+    
+    options.mu0       = 0e-0;  % An absolute value
+    options.wait      = 0;     
+    options.MaxIter   = 50;
+    options.gamma     = 1e-16;
+    
+    options.FeaTol    = 1e-7;
+    options.OptTol    = 1e-7;
+    options.LSMRMaxIter = 100;
+    
+    gamma = 0.0;
+    delta = 0.0;
+    
+    for j = 1:no_solvers
+        %!!!!!!!!!!!!!!!!!!!!!!!!!!%
+        %! Loop over the solvers  !%
+        %!!!!!!!!!!!!!!!!!!!!!!!!!!%
+        
+        d1      = gamma;      % Can be scalar if D1 = d1*I.
+        d2      = delta*ones(m,1);
+        options.Method = test(j).Method;
+        options.krylov_method = test(j).krylov_method;
+        options.precond_method = test(j).precond_method;
+        options.LSMRatol1 = test(j).inner_tol;
+        options.LSMRatol2 = test(j).inner_tol;
+          
+        d1      = gamma;      % Can be scalar if D1 = d1*I.
+        d2      = delta*ones(m,1);
+        [x,y,z,inform,PDitns{j,i},CGitns{j,i},time{j,i},iv,extras{j,i}] = ...
+            pdco(c,A,b,bl,bu,d1,d2,options,x0, ...
+                     y0,z0,xsize,zsize );
+        itvec{j,i} = iv(1:PDitns{j,i});
+        
+        lp_post;
+    end
+end
+
+lp_runs_writetable;
+
+
+plot_convergence(no_solvers,extras,test,1)
--- paper_lp_runs.m
+++ paper_lp_runs.m
@@ -14,7 +14,7 @@ max_sigma(length_problem) = 0;
 %!! S E T U P    T E S T S  !!
 %!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-no_solvers = 3;
+no_solvers = 4;
 test = struct('Method',{},...
               'krylov_method',{},...
               'precond_method',{},...
@@ -38,15 +38,15 @@ test(2).inner_tol = 1e-1;
 test(2).PDConvergenceDescriptions = 'aug_lag';
 test(2).descriptions = ['Aug lag, tol = $10^-1\\eta_k$'];
 
-% setup second test
+% setup third test
 test(3).Method = 23;        % krylov solver
 test(3).krylov_method = 11;  % PPCG
 test(3).precond_method = 4; % constraint pre
 test(3).inner_tol = 1e-2;   
 test(3).PDConvergenceDescriptions = 'PPCG';
-test(3).descriptions = ['PPCG, tol = $10^-1\\eta_k$'];
+test(3).descriptions = ['PPCG, tol = $10^-2\\eta_k$'];
 
-% setup second test
+% setup fourth test
 test(4).Method = 23;        % krylov_solver
 test(4).krylov_method = 4;  % mpgmres
 test(4).precond_method = 1; % aug lag
@@ -55,59 +55,4 @@ test(4).PDConvergenceDescriptions = 'aug_lag_tiny_mu';
 test(4).descriptions = ['Aug lag, tol = $10^-7$'];
 
 
-for i = 1:length_problem
-
-    %!!!!!!!!!!!!!!!!!!!!!!!!!!%
-    %! Loop over the problems !%
-    %!!!!!!!!!!!!!!!!!!!!!!!!!!%
-    probstring = ['/numerical/trees/matrices/netlib_problems/',problem{i}];
-    %    load(problem{i});
-    load(probstring);
-    
-    lp_setup;
-    
-    max_sigma(i) = svds(A,1)
-    
-    conv_data.max_sigma = max_sigma(i);
-    options.conv_data = conv_data;
-    
-    options.mu0       = 0e-0;  % An absolute value
-    options.wait      = 0;     
-    options.MaxIter   = 50;
-    options.gamma     = 1e-16;
-    
-    options.FeaTol    = 1e-7;
-    options.OptTol    = 1e-7;
-    options.LSMRMaxIter = 100;
-    
-    gamma = 0.0;
-    delta = 0.0;
-    
-    for j = 1:no_solvers
-        %!!!!!!!!!!!!!!!!!!!!!!!!!!%
-        %! Loop over the solvers  !%
-        %!!!!!!!!!!!!!!!!!!!!!!!!!!%
-        
-        d1      = gamma;      % Can be scalar if D1 = d1*I.
-        d2      = delta*ones(m,1);
-        options.Method = test(j).Method;
-        options.krylov_method = test(j).krylov_method;
-        options.precond_method = test(j).precond_method;
-        options.LSMRatol1 = test(j).inner_tol;
-        options.LSMRatol2 = test(j).inner_tol;
-          
-        d1      = gamma;      % Can be scalar if D1 = d1*I.
-        d2      = delta*ones(m,1);
-        [x,y,z,inform,PDitns{j,i},CGitns{j,i},time{j,i},iv,extras{j,i}] = ...
-            pdco(c,A,b,bl,bu,d1,d2,options,x0, ...
-                     y0,z0,xsize,zsize );
-        itvec{j,i} = iv(1:PDitns{j,i});
-        
-        lp_post;
-    end
-end
-
-lp_runs_writetable;
-
-
-plot_convergence(no_solvers,extras,test,1)
+lp_test_loop;

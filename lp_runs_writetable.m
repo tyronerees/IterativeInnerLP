4e3cdee159f581e6112f7332c9ab114ae001f95e
Tyrone Rees <tyrone.rees@stfc.ac.uk>
2015-08-10 12:18:15 +0100

    Edited the table printing section so that it prints accrately

--- lp_runs_writetable.m
+++ lp_runs_writetable.m
@@ -4,7 +4,7 @@ for i = 1:no_solvers
 end 
 fprintf('\\\\ \n');
 
-for i = 1:length_problem
+for i = first_prob:first_prob + length_problem - 1
     fprintf(problem{i}(1:end-4)); % remove the .mat
     for j = 1:no_solvers
         fprintf(' & %i',PDitns{j,i});

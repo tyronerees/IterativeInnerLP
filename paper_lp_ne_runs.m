0837c81d046e5ed6a09ac13bd13965ea93dd62bd
Tyrone Rees <tyrone.rees@stfc.ac.uk>
2015-08-14 11:44:16 +0100

    Do error runs for ne

--- paper_lp_ne_runs.m
+++ paper_lp_ne_runs.m
@@ -7,6 +7,8 @@ close all
 problem = importdata('netlib.txt');
 length_problem = length(problem);
 
+imgname = 'ne_runs_';
+
 first_prob = 1;
 length_problem = 1;
 
@@ -16,7 +18,7 @@ max_sigma(length_problem) = 0;
 %!! S E T U P    T E S T S  !!
 %!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-no_solvers = 3;
+no_solvers = 4;
 test = struct('Method',{},...
               'krylov_method',{},...
               'precond_method',{},...
@@ -34,15 +36,27 @@ test(1).descriptions = '$A\backslash b$';
 
 % setup second test
 test(2).Method = 5;        % pcg
-test(2).inner_tol = 1e-2;    
-test(2).PDConvergenceDescriptions = 'CG';
-test(2).descriptions = ['Aug lag, tol = $10^{-2}$'];
+test(2).inner_tol = 1e-1    
+test(2).PDConvergenceDescriptions = 'CG, tol 10^-4';
+test(2).descriptions = ['Norm Eqns, tol = $10^{-4}$'];
 
 % setup third test
 test(3) = test(2);
-test(3).inner_tol = 1e-5;   
-test(3).PDConvergenceDescriptions = 'CG, tighter tol';
-test(3).descriptions = ['Aug lag, tol = $10^-8$'];
+test(3).inner_tol = 1e-3;   
+test(3).PDConvergenceDescriptions = 'CG, tol 10^-6';
+test(3).descriptions = ['Norm Eqns, tol = $10^-6$'];
+
+% setup third test
+test(4) = test(2);
+test(4).inner_tol = 1e-5;   
+test(4).PDConvergenceDescriptions = 'CG, tol 10^-8';
+test(4).descriptions = ['Norm Eqns, tol = $10^-8$'];
+
+% $$$ % setup third test
+% $$$ test(5) = test(2);
+% $$$ test(5).inner_tol = 1e-7;  
+% $$$ test(5).PDConvergenceDescriptions = 'CG, tol 10^-10';
+% $$$ test(5).descriptions = ['Norm Eqns, tol = $10^-10$'];
 
 
 lp_test_loop;
